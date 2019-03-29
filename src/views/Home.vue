f1a079d0a19a072211d4e5ce0ea53e6c482c185a
Bianca <bianca.rocha1711@gmail.com>
2019-03-29 10:32:56 -0300

    funtime! Applying styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
   <div data-test="home-component" class="home">
-    <span data-test="count-display">{{ count }}</span>
-    <button @click="increment" data-test="increment-button">+</button>
-    <button @click="decrement" data-test="decrement-button">-</button>
-    <span data-test="feedback-message">{{ feedback }}</span>
+    <div class="home-display">
+      <div class="home-title">Welcome to The Magical Counter</div>
+      <span data-test="count-display">{{ count }}</span>
+      <div class="feedback-message" data-test="feedback-message">
+        {{ feedback }}
+      </div>
+    </div>
+    <div class="home-buttons">
+      <button
+        class="home-button decrement"
+        @click="decrement"
+        data-test="decrement-button"
+      >
+        <span>-</span>
+      </button>
+      <button
+        class="home-button increment"
+        @click="increment"
+        data-test="increment-button"
+      >
+        <span>+</span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -31,3 +50,74 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+// colors
+$bg: #eef0f0;
+$display: #98c865;
+$text: #425463;
+$button-bg: #fa6b5b;
+$button-color: #eef0f0;
+
+body {
+  margin: 0;
+  background: #acd1de;
+}
+.home {
+  display: inline-block;
+  background: $bg;
+  border-radius: 20px;
+  margin-top: 10%;
+  color: $text;
+  padding: 10px;
+  font-family: "VT323", monospace;
+  .home-display {
+    width: 300px;
+    height: 200px;
+    border-radius: 20px;
+    margin: 0 auto;
+    background: $display;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    span {
+      font-size: 128px;
+    }
+  }
+  .home-title {
+    margin: 5px;
+  }
+  .home-buttons {
+    width: 300px;
+    text-align: center;
+    margin: 20px auto;
+  }
+  .home-button {
+    background: $button-bg;
+    color: $button-color;
+    border: none;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin: 0 45px;
+    cursor: pointer;
+    font-size: 45px;
+    &.increment {
+      span {
+        position: relative;
+        left: 1px;
+      }
+    }
+    &.decrement {
+      span {
+        position: relative;
+        top: -1px;
+      }
+    }
+  }
+  .feedback-message {
+    margin: 5px;
+    min-height: 20px;
+  }
+}
+</style>
